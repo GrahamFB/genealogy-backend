b1fc63bb00f28e41125dc9b7f30322d034bea525
Simon B <simon.bragg+github@gmail.com>
2018-11-04 16:16:42 +1100

    Change server port tp 4000 to not clash with frontend

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
  * @Author: Simon Bragg
  * @Date: 2018-11-04 15:14:29 
  * @Last Modified by: Simon B
- * @Last Modified time: 2018-11-04 15:51:15
+ * @Last Modified time: 2018-11-04 16:16:20
  */
 
 require('dotenv').config()
 
 const server = require('./server')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => console.log(`Genealogy API server started on ${port}`))
