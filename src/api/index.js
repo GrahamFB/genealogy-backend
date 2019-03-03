a1cdac3732a27d37d36873d49ed1f1f07021c417
Simon B <simon.bragg+github@gmail.com>
2019-03-03 12:14:35 +1100

    Added Events Route

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 
 const { errorHandler } = require('../middleware');
+
 // list of models here
 const { Question } = require('../models/question');
+const { Event } = require('../models/event')
 
 // list of controllers here
 const questions = require('../controllers/questions');
+const events = require('../controllers/events');
 
 // combine models ino one object
-const models = { Question };
+const models = { Question, Event };
 
 const routersInit = config => {
   const router = express();
 
   // register api points
   router.use('/questions', questions(models, { config }));
+  router.use('/events', events(models, { config }));
 
   // catch api all errors
   router.use(errorHandler);
