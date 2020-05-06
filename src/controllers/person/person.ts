c090afc2d8c3392a27fc4af1255571690026bd1b
Simon Bragg <simon.bragg+github@gmail.com>
2020-05-06 16:17:50 +1000

    minor update

--- src/controllers/person/person.ts
+++ src/controllers/person/person.ts
@@ -3,45 +3,67 @@ import { Person } from "../../models/person";
 import _ from "lodash";
 
 // Create and Save a new Person
-export const create = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const person = new Person();
+export const create = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const person = new Person();
 
-        _.extend(person, req.body);
+    _.extend(person, req.body);
 
-        await person.save();
-    
-        res.send(person);
-      } catch (error) {
-        next(error);
-      }
+    await person.save();
+
+    res.send(person);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // Retrieve all Persons from the database.
-export const findAll = async (req: Request, res: Response) => {
-    const { limit, skip } = req.query;
+export const findAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { limit, skip } = req.query;
 
-    const s = skip ? parseInt(skip as string, 10) : 0;
-    const l = limit ? parseInt(limit as string, 10) : 100;
+  const s = skip ? parseInt(skip as string, 10) : 0;
+  const l = limit ? parseInt(limit as string, 10) : 100;
 
-    const persons = await Person.find({})
-      .sort({ _id: -1 })
-      .skip(s)
-      .limit(l);
-    res.send(persons);
+  try {
+    const persons = await Person.find({}).sort({ _id: -1 }).skip(s).limit(l);
+    
+    res.status(200).send(persons);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // Find a single Person with an id
-export const findOne = (req: Request, res: Response) => {
-    res.send("findOne()...");
+export const findOne = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const person = await Person.findById(id);
+
+    res.status(200).send({ person });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // Update a Person by the id in the request
 export const update = (req: Request, res: Response) => {
-    res.send("update()...");
+  res.send("update()...");
 };
 
 // Delete a Person with the specified id in the request
 export const remove = (req: Request, res: Response) => {
-    res.send("remove()...");
+  res.send("remove()...");
 };
