af7a3fc7e42991e7e3ac6989dfd015304ed26346
Graham Bragg <Graham@Grahams-MBP.hub>
2020-06-07 20:05:05 +1000

    Add additional ENUMS

---
+++ src/models/enums/placeType.ts
@@ -0,0 +1,8 @@
+export enum PlaceType {
+    ADDRESS = "https//freedomsloom.org/v1/enumerations/place-type/Address",
+    CITY = "https//freedomsloom.org/v1/enumerations/place-type/City",
+    COUNTY = "https//freedomsloom.org/v1/enumerations/place-type/County",
+    PROVINCE = "https//freedomsloom.org/v1/enumerations/place-type/Province",
+    STATE = "https//freedomsloom.org/v1/enumerations/place-type/State",
+    COUNTRY = "https//freedomsloom.org/v1/enumerations/place-type/Country"
+}
--- src/relationship/schemas/relationship.schema.ts
+++ src/relationship/schemas/relationship.schema.ts
@@ -2,7 +2,7 @@ import * as mongoose from "mongoose";
 
 export const RelationshipSchema = new mongoose.Schema({
     type: String,
-    person1: String,
-    person2: String,
+    person1: mongoose.Types.ObjectId,
+    person2: mongoose.Types.ObjectId,
     facts: Array
 }, { timestamps: true });
